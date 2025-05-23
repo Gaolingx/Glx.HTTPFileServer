c66042a608d46084aae2d9c4213aa8de97c65313
Gaolingx <947770192@qq.com>
2025-05-23 15:55:27 +0800

    2025052301

---
+++ HTTPFileServer/https_server.py
@@ -0,0 +1,33 @@
+import http.server
+import ssl
+from functools import partial
+
+# 定义服务器地址和端口
+server_ip = '0.0.0.0'
+server_port = 26661
+server_address = (server_ip, server_port)
+target_directory = "F:/FileCDN"
+
+# 创建SSL上下文
+context = ssl.SSLContext(ssl.PROTOCOL_TLS_SERVER)
+context.load_cert_chain(certfile='./keys/cert.pem', keyfile='./keys/key.pem')
+
+# 创建自定义Handler
+handler = partial(http.server.SimpleHTTPRequestHandler, directory=target_directory)
+
+# 创建HTTPServer实例
+httpd = http.server.HTTPServer(server_address, handler)
+
+# 包装SSL套接字
+httpd.socket = context.wrap_socket(httpd.socket, server_side=True)
+
+print(f"HTTPS服务器已启动，访问：https://{server_ip}:{server_port}")
+
+try:
+    # 启动服务器
+    httpd.serve_forever()
+except KeyboardInterrupt:
+    print("\n收到中断信号，正在关闭服务器...")
+    httpd.shutdown()  # 停止服务器循环
+    httpd.server_close()  # 关闭底层socket
+    print("HTTP服务器已停止。")
